96d4bd25efa2dc0f175ecac2e7c6659f4fda7f2f
DionPi <deathwing1999@gmail.com>
2026-06-28 12:54:42 +0300

    feat: add new evaluation functions for models

---
+++ test.py
@@ -0,0 +1,100 @@
+import os
+import pickle
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn import feature_selection
+from sklearn.metrics import confusion_matrix, zero_one_loss, classification_report, mutual_info_score
+from utils import concatenate_data, split_features_from_target, load_model, get_feature_dimensionality, has_preprocessor
+from train import (logistic_regression_text_features,
+                   logistic_regression_one_hot,
+                   svm_one_hot,
+                   svm_text_features)
+
+full_dataset = [
+                "part1.xml",
+                "part2.xml",
+                "part3.xml",
+                "part4.xml",
+                "part5.xml",
+                "part6.xml",
+                "part7.xml",
+                "part8.xml",
+                "part9.xml",
+                "part10.xml"
+               ]
+
+
+def build_model(function, train_set, **kwargs):
+
+    path = function(train_set, **kwargs)
+
+    return path
+
+
+def plot_conf_matrix(y, yhat, model_type, metrics_dir='metrics'):
+    # 1. Plot the Distribution of Predictions
+    plt.figure(figsize=(10, 4))
+
+    plt.subplot(1, 2, 1)
+    sns.countplot(x=yhat, palette='viridis')
+    plt.title('Predicted Class Distribution')
+    plt.xlabel('Sentiment Class')
+    plt.ylabel('Count')
+
+    # 2. Plot Confusion Matrix (Comparing preds to true labels y)
+    plt.subplot(1, 2, 2)
+    cm = confusion_matrix(y, yhat)
+    labels = [l if l else 'Broken' for l in model.classes_]
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                xticklabels=labels,  # Adjust to your classes
+                yticklabels=labels)
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(metrics_dir, f'{model_type}_confusion_matrix.png'))
+
+    return plt
+
+
+def evaluate_model(clf, *test_set:str):
+
+    if isinstance(clf, str):
+        if 'models/' in clf: clf = load_model(clf)
+        else: clf = load_model(os.path.join('models', clf))
+
+    df = concatenate_data(list(test_set))
+    key = 'one-hot' if has_preprocessor(clf) else 'text_f'
+
+    classifier_step = clf.named_steps["classifier"]
+    model_type_str = type(classifier_step).__name__ + "_" + key
+    print(f'Model Type: {model_type_str}')
+
+    X, y = split_features_from_target(df, key)
+
+    preds = clf.predict(X)
+    if preds.ndim > 1:
+        preds = np.argmax(preds, axis=1)
+
+    cm = plot_conf_matrix(y, preds, model_type_str)
+    cm.show()
+
+    clr = classification_report(y, preds, output_dict=True)
+
+    return clr['accuracy']
+
+
+test_files = ["part1.xml"]
+train_files = [f for f in full_dataset if f not in test_files]
+
+X_test, y_test = split_features_from_target(*test_files)
+
+model = load_model("models/svm_onehot_ngram_(1, 3)_max_iter_1000_C_1.0.pkl")
+
+evaluate_model(model, 'part3.xml')
+
+print(get_feature_dimensionality(model))
